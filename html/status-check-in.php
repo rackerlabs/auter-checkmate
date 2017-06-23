edfe010141b27979fef49e49bbb8257c490752f8
Ruth Lee <ruth@ruthealee.co.uk>
2017-06-23 12:37:41 -0400

    added the initial php skeleton

---
+++ html/status-check-in.php
@@ -0,0 +1,37 @@
+<html>
+<title> sample </title>
+<body>
+<?php
+$method = $_SERVER['REQUEST_METHOD'];
+if ('PUT' === $method) {
+    parse_str(file_get_contents('php://input'), $_put_vars);
+    echo $_put_vars['server-id']." is serverid\n";
+    echo $_put_vars['status-id']." is server status\n";
+
+    $servername = "localhost";
+    $username = "updater";
+    $password = "access";
+    $dbname = "auter";
+    $sql = "SELECT * from status;";
+    $insertsql = "UPDATE status SET statusid=:passedstatus where serverid=:passedserver";
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        $stmt = $conn->prepare($insertsql);
+        echo $_put_vars['server-id']." is serverid\n";
+        $stmt->bindValue(':passedstatus', $_put_vars['status-id'], PDO::PARAM_INT);
+        $stmt->bindValue(':passedserver', $_put_vars['server-id'], PDO::PARAM_STR);
+        $stmt->execute();   
+	
+        
+
+}
+
+
+else {
+echo "<p>Please submit as a PUT request</p>";
+}
+?>
+</body>
+</html>
+
