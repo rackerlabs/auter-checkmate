d034c418d51e90f42fb0d800ffa74eeff81b977f
Ruth Lee <ruth.ea.lee@gmail.com>
2017-06-30 15:19:40 +0000

    started adding some error reporting

--- html/status-check-in.php
+++ html/status-check-in.php
@@ -1,24 +1,34 @@
 <?php
 include './auter_checkmate_cred.php';
-$method = $_SERVER['REQUEST_METHOD'];
-if ('PUT' === $method) {
-    parse_str(file_get_contents('php://input'), $_put_vars);
-    $sql = "SELECT * from statuses;";
-    $insertsql = "UPDATE serverstatus SET statusid=:passedstatus where serverid=:passedserver";
+
+function update_status($_put_vars) {
+    include './auter_checkmate_cred.php';
+      try {
+        $insertsql = "UPDATE serverstatus SET statusid=:passedstatus where serverid=:passedserver";
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         $stmt = $conn->prepare($insertsql);
         $stmt->bindValue(':passedstatus', $_put_vars['status-id'], PDO::PARAM_INT);
         $stmt->bindValue(':passedserver', $_put_vars['server-id'], PDO::PARAM_STR);
-        $stmt->execute();   
-	
-        
-
+        $stmt->execute();
+        $no_update = $stmt->rowCount();
+	if ($no_update === 0) {
+	  header('No change', true, 501);
+          echo "No changes made, either not registered, invalid status or no change\n";
+	}
+      }
+      catch(Exception $e) {
+        echo "Should have used MongoDB brah:  " .$e->getMessage();
+      }
 }
-
-
+$method = $_SERVER['REQUEST_METHOD'];
+if ('PUT' === $method) {
+    parse_str(file_get_contents('php://input'), $_put_vars);
+    update_status($_put_vars);
+}
 else {
-echo "<p>Please submit as a PUT request</p>";
+    header('Please submit a put request', true, 405);
+    echo "<p>Please submit as a PUT request</p>";
 }
 ?>
